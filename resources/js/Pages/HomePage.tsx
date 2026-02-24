da608111ec483d9bdecea11193d7cf78a904a211
NionGeraghty <geraghnb@tcd.ie>
2026-02-24 10:50:52 +0100

    All the tech is set up and talking to each other. Ready to start building

---
+++ resources/js/Pages/HomePage.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function HomePage() {
+    return (
+        <div className="container">
+            <h1>Dev diary</h1>
+            <p>Hello world!</p>
+        </div>
+    );
+}
