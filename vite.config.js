a8be81a51c32527ab6721fd259de2bc0b571981e
NionGeraghty <geraghnb@tcd.ie>
2026-03-09 12:48:31 +0100

    Change to deploy on Railway

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
         tailwindcss(),
@@ -16,11 +16,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
-        },
-    },
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
         },
     },
 });
